e4b67ffda54fce0d1e0759b68e739cf259a4e779
iamakhilvarun <iamakhilvarun@gmail.com>
2026-08-12 02:17:54 +0530

    two polynomial sum

--- 5_08_2026/3_3.c
+++ 5_08_2026/3_3.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int degree;
+    int poly1[100];
+    int poly2[100];
+    int sum[100];
     
-}
+        printf("Enter the maximum no of degree: ");
+        scanf("%d", &degree);
+
+        //input of the polynomial 1
+        for (int i = 0; i <= degree; i++)
+        {
+            printf("Enter the coefficient of poly1 x^%d: ",i);
+            scanf("%d", &poly1[i]);
+        }
+        printf("\n");
+        //input of the polynomial 2
+        for (int i = 0; i <= degree; i++)
+        {
+            printf("Enter the coefficient of poly2 x^%d: ",i);
+            scanf("%d", &poly2[i]);
+        }
+
+        // code for sum
+        for (int i = 0; i <= degree; i++)
+        {
+            sum[i]=poly1[i]+poly2[i];
+        }
+
+        // printing the sum of the polynomial
+        for(int i=0;i<=degree;i++){
+            printf("%d",sum[i]);
+
+            if (i>0){
+                printf("x^%d",i);
+            }
+            if (i<degree){
+                printf("+");
+            }
+        }
+        
+        return 0;
+    }
